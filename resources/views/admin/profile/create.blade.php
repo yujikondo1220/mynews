65a8601d58e70a239dce1209963e9288cd757d96
yujikondo <yujikondo1220@gmail.com>
2020-07-25 19:50:40 +0000

    課題４

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MyNews</title>
+    </head>
+@extends('layouts.profile')
+
+@section('title', 'プロフィール')
+
+@section('content')
+ <div class="container">
+     <div class="row">
+         <div class="col-md-8 mx-auto">
+             <h2>My プロフィール</h2>
+         </div>
+     </div>
+ </div>
+@endsection
+    </body>
+</html
