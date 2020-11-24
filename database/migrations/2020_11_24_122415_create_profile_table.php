5d2a9aeaf0e051ea49b392e5323c20fbb9cb942c
yujikondo <yujikondo1220@gmail.com>
2020-11-24 13:59:43 +0000

    second commit

--- database/migrations/2020_11_24_122415_create_profile_table.php
+++ database/migrations/2020_11_24_122415_create_profile_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProfilesTable extends Migration
+class CreateProfileTable extends Migration
 {
     /**
      * Run the migrations.
@@ -30,6 +30,6 @@ class CreateProfilesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('profiles');
+        Schema::dropIfExists('profile');
     }
 }
